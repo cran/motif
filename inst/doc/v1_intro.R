b4e0ff4e7b79e8caee7e91270eaefda3bdf75515
Jakub Nowosad <nowosad.jakub@gmail.com>
2025-07-26 08:20:02 +0000

    version 0.6.5

--- inst/doc/v1_intro.R
+++ inst/doc/v1_intro.R
@@ -1,4 +1,4 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>",
@@ -12,7 +12,7 @@ library(stars)
 library(sf)
 landcover = read_stars(system.file("raster/landcover2015.tif", package = "motif"))
 
-## ---- echo=FALSE--------------------------------------------------------------
+## ----echo=FALSE---------------------------------------------------------------
 landcover = droplevels(landcover)
 plot(landcover, key.pos = 4, key.width = lcm(5), main = NULL)
 
@@ -26,7 +26,7 @@ landcover_comp
 ## -----------------------------------------------------------------------------
 landcover_comp$signature
 
-## ---- echo=FALSE--------------------------------------------------------------
+## ----echo=FALSE---------------------------------------------------------------
 stars200 = lsp_add_sf(landcover, window = 200)
 landcover = droplevels(landcover)
 plot(landcover, key.pos = NULL, reset = FALSE, main = NULL)
@@ -44,7 +44,7 @@ landcover_coma$signature[[1]]
 ## -----------------------------------------------------------------------------
 ecoregions = read_sf(system.file("vector/ecoregions.gpkg", package = "motif"))
 
-## ---- echo=FALSE--------------------------------------------------------------
+## ----echo=FALSE---------------------------------------------------------------
 # https://medialab.github.io/iwanthue/
 my_pal = c("#d34359", "#62b93c", "#b75fcf", "#53c069", "#d44295", "#acb939", "#626edd", "#dc9e36", "#8156a8", "#4b8734", "#d98dc7", "#58c096", "#cc542a", "#48bbd2", "#bf814d", "#6686c8", "#968c30", "#a34d78", "#36815b", "#c26963", "#a2b36b", "#6b6829")
 plot(ecoregions["id"], main = NULL, col = my_pal, key.pos = 4, key.width = lcm(5))
